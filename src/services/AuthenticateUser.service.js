e08a10563ca1b1eade7413485145511699b4ccf4
franzinBr <alan-franzin2011@live.com>
2021-07-02 10:33:40 -0400

    Added Authenticate User Service

---
+++ src/services/AuthenticateUser.service.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const { compare } = require('bcryptjs');
+const ErrorResponse = require('../utils/ErrorResponse');
+const GenAuthTokenService = require('./GenAuthToken.service');
+const GenRefreshTokenService = require('./GenRefreshToken.service');
+
+class AuthenticateUserService {
+    async execute(userOrEmail, password) {
+        if (!userOrEmail || !password)
+            throw new ErrorResponse('Invalid Credentials', 400);
+
+        let user = await User.findOne({}, '+password').or([
+            { email: userOrEmail },
+            { username: userOrEmail },
+        ]);
+        if (!user) throw new ErrorResponse('Invalid Credentials', 400);
+
+        const passwordMatch = await compare(password, user.password);
+        if (!passwordMatch) throw new ErrorResponse('Invalid Credentials', 400);
+
+        const genAuthTokenService = new GenAuthTokenService();
+        const genRefreshTokenService = new GenRefreshTokenService();
+
+        const authToken = await genAuthTokenService.execute(
+            user.username,
+            user._id
+        );
+        const refreshToken = await genRefreshTokenService.execute(
+            user.username,
+            user._id
+        );
+
+        return {
+            authToken,
+            refreshToken,
+            userInfo: {
+                id: user._id,
+                name: user.fullname,
+                username: user.username,
+                admin: user.admin,
+            },
+        };
+    }
+}
+
+module.exports = AuthenticateUserService;
