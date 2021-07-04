611d07c42aad9462325be38906bd702ace5aef3a
franzinBr <alan-franzin2011@live.com>
2021-07-03 22:18:29 -0400

    Added server file

---
+++ server.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const Database = require('./src/configs/database');
+const router = require('./src/routes');
+const ErrorHandler = require('./src/middlewares/errorHandler');
+
+Database.connect();
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan('common'));
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+app.use(cors({ origin: process.env.FRONT_URL, credentials: true }));
+app.use(cookieParser());
+app.use(router);
+
+app.use(ErrorHandler.handle);
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('[NODE PRODUCTION]');
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 3080;
+
+const server = app.listen(PORT, () => console.log(`Server running on ${PORT}`));
