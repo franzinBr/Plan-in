f0f42185aeb5434dd0e9cf0965b296e76c0f75db
franzinBr <alan-franzin2011@live.com>
2021-07-04 17:48:50 -0400

    Added default theme

---
+++ client/src/themes/default.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        background: '#FAFAFA',
+        text: '#262626',
+        text_second: '#787680',
+        primary: '#0097e6',
+        primary_plus: '#004b73',
+        header: '#FFFFFF',
+    },
+    sizes: {
+        height_header: '8rem',
+    },
+};
+
+export default theme;
