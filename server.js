f26e9daeaf0c046b3a3becb7061ceaa05a6f7921
franzinBr <alan-franzin2011@live.com>
2021-07-05 12:17:59 -0400

    fix paths

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const Database = require('./src/configs/database');
