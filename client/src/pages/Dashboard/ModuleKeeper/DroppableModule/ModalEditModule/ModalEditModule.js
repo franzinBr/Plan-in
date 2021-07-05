6169ef7d32441650fca91493dcd162499dfba042
franzinBr <alan-franzin2011@live.com>
2021-07-05 10:15:55 -0400

    Created Modal Edit Module

---
+++ client/src/pages/Dashboard/ModuleKeeper/DroppableModule/ModalEditModule/ModalEditModule.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Modal from '../../../../../components/Modal/Modal';
+
+import Button from '../../../../../components/Form/Button/Button';
+import Input from '../../../../../components/Form/Input/Input';
+import { useForm } from 'react-hook-form';
+import { Form, ErrorForm } from './ModalEditModule.style';
+import { delay } from '../../../../../utils/delay';
+import { editModuleRequest } from '../../../../../services/module';
+
+const ModalEditModule = ({ showModal, setShowModal, setModules, module }) => {
+    const [success, setSuccess] = React.useState(null);
+    const [error, setError] = React.useState(null);
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        try {
+            const res = await editModuleRequest(module._id, {
+                name: data.name,
+            });
+            if (!res.data.success) throw new Error(res.data.message);
+
+            module.name = res.data.moduleEdited.name;
+            module.updatedAt = res.data.moduleEdited.updatedAt;
+            setModules((modules) => [...modules]);
+            reset();
+            setSuccess(true);
+            await delay(500);
+            setShowModal(false);
+            setSuccess(false);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    return (
+        <Modal
+            onSubmit={handleSubmit(onSubmit)}
+            showModal={showModal}
+            setShowModal={setShowModal}
+            reset={reset}
+            success={success}
+        >
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    label="Rename Module"
+                    type="text"
+                    name="name"
+                    defaultValue={module.name}
+                    register={register}
+                    error={errors['name']}
+                />
+                <Button>Edit</Button>
+                {error && <ErrorForm>{error}</ErrorForm>}
+            </Form>
+        </Modal>
+    );
+};
+
+export default ModalEditModule;
---
+++ client/src/pages/Dashboard/ModuleKeeper/DroppableModule/ModalEditModule/ModalEditModule.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+    padding: 4rem;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+`;
+
+export const ErrorForm = styled.p`
+    padding-top: 0.5rem;
+    font-size: 1.5rem;
+    color: #ff0033;
+`;
