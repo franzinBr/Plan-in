0cdf4df33f3fa62dadbe32cdc7c5226cdc7b7a12
franzinBr <alan-franzin2011@live.com>
2021-07-05 21:51:30 -0400

    Update endpoints

--- client/src/services/module.js
+++ client/src/services/module.js
@@ -1,26 +1,26 @@
 import { api } from './api';
 
 export async function listModulesRequest() {
-    const res = await api.get('/module');
+    const res = await api.get('/modules');
     return res;
 }
 
 export async function listModulesAllRequest() {
-    const res = await api.get('/module/all');
+    const res = await api.get('/modules/all');
     return res;
 }
 
 export async function createModulesRequest(module) {
-    const res = await api.post('/module', module);
+    const res = await api.post('/modules', module);
     return res;
 }
 
 export async function editModuleRequest(id, editedModule) {
-    const res = await api.put(`/module/${id}`, editedModule);
+    const res = await api.put(`/modules/${id}`, editedModule);
     return res;
 }
 
 export async function deleteModuleRequest(id) {
-    const res = await api.delete(`/module/${id}`);
+    const res = await api.delete(`/modules/${id}`);
     return res;
 }
