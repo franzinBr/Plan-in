aa60de83edf462981e08830a2fc39063cac2c1c6
franzinBr <alan-franzin2011@live.com>
2021-07-06 13:29:55 -0400

    remove logs

--- src/controllers/RefreshAuthToken.controller.js
+++ src/controllers/RefreshAuthToken.controller.js
@@ -3,7 +3,6 @@ const RefreshAuthTokenService = require('../services/RefreshAuthToken.service');
 class RefreshAuthTokenController {
     async handle(req, res) {
         const refreshToken = req.cookies.refreshtoken;
-        console.log(req.cookies);
 
         const refreshAuthTokenService = new RefreshAuthTokenService();
         const { authToken, userInfo: user } =
